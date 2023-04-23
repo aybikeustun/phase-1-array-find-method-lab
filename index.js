92a860d0ca3d3bb275a69b4d5df39526983d9d89
aybikeustun <aybikeustun@gmail.com>
2023-04-23 19:06:46 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(records) {
+    const winRecord = records.find(record => record.result === "W");
+    if (winRecord) {
+      return winRecord.year;
+    } else {
+      return undefined;
+    }
+  }
+  
+  
